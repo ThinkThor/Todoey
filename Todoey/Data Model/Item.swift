6c7746a6d12fe995a8e8984753b55bf9a129cf4f
Thor Henrik Moen <thorhenrikmoen@thors-macbook-pro.local>
2018-01-28 15:15:29 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Thor Henrik Moen on 28.01.2018.
+//  Copyright © 2018 Thor Henrik Moen. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
